0015577a8eb8096a1bea4a58b7926c1727c04453
Zibnitskii Aleksandr <a.zibnitskiy@gmail.com>
2022-09-16 19:47:00 +0300

    added some files

---
+++ DevNet-Python-APIC-EM/task/1-2-3-3 Activity - Troubleshoot List and Dictionary Code/02_list-dicts.py
@@ -0,0 +1,25 @@
+# Create a list of the BRICS countries
+country = [ 
+            "Brazil", 
+            "Russia", 
+            "India", 
+            "China", 
+            "South Africa"
+          ]
+
+capitals = {
+    "South Africa": [
+                        "Pretoria",
+                        "Cape Town",
+                        "Bloemfontein"
+                    ],
+    "India": "New Delhi",
+    "China": "Beijing",
+    "Russia": "Moscow",
+    "Brazil": "Brasilia"
+           }
+
+# Print the list and dictionary
+print( country )
+print( capitals )
+print( capitals["South Africa"][1] )
